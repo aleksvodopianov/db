a5af0ed107de10b941055844c4403dd5b7cae294
Aleks <vodopyanov3d@gmail.com>
2021-04-07 22:49:58 +0300

    create cards

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,47 @@ testWebP(function (support) {
         console.log('not use webp');
     }
 });
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  function req() {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:6060/people');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+    request.addEventListener('load', function () {
+      if (request.status == 200) {
+        let data = JSON.parse(request.response);
+        console.log(data);
+
+        data.forEach(item => {
+          let card = document.createElement('div');
+
+          card.classList.add('card');
+
+          let icon;
+          if (item.sex === 'male') {
+            icon = 'icons/mars.png';
+          } else {
+            icon = 'icons/female.png';
+          }
+
+          card.innerHTML = `
+                        <img src="${item.photo}" alt="${item.name}">
+                        <div class="name">${item.name} ${item.surname}</div>
+                        <div class="sex">
+                            <img src=${icon} alt="${item.sex}">
+                        </div>
+                        <div class="age">${item.age}</div>
+                    `;
+          document.querySelector('.app').appendChild(card);
+        });
+      } else {
+        console.error('Что-то пошло не так!');
+      }
+    });
+    this.remove();
+  }
+  document.querySelector('button').addEventListener('click', req, { once: true });
+});
